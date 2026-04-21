d4e8817e4530f0ce9bce9f6f48f2c1eb09388dc6
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2026-04-21 11:04:50 +0100

    restaurant challenge

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,13 @@
 #include <spdlog/spdlog.h>
 #include <string>
 
-#include "user/creator.hpp"
-#include "user/user.hpp"
-
 namespace {
 
 struct Args
 {
-  std::string username;
 };
 
-void run(const Args &args)
-{
-  const auto userCreator{ usr::createCreator() };
-  const usr::User user{ userCreator->createUser(args.username) };
-
-  spdlog::info("User created with id: {}, name: {}", user.id(), user.name());
-}
+void run() {}
 
 }// namespace
 
@@ -28,13 +18,9 @@ int main(int argc, const char **argv)
   try {
     CLI::App app{ "A simple example project" };
 
-    Args args;
-
-    app.add_option("--username", args.username, "The username you would like to use.")->required();
-
     CLI11_PARSE(app, argc, argv);
 
-    run(args);
+    run();
 
   } catch (const std::exception &e) {
     spdlog::error("Unhandled exception in main: {}", e.what());
